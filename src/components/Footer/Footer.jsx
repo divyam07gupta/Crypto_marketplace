4c65588af0041cd8c8c66b39b67c95b66435c1f6
Divyam Gupta <gdivyam0234684@gmail.com>
2024-07-06 02:08:34 +0530

    crypto

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <p>Copyright @ 2024, Crypto Market - All Rights Reserved</p>
+      <p>Divyam Gupta</p>
+    </div>
+  )
+}
+import './Footer.css'
+
+export default Footer
